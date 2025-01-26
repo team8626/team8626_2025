ef4c9c2ddd7d1fdee3386e8f978eb6eef07e1c55
Sylvain Dumet <Sylvain.Dumet@gmail.com>
2025-01-26 17:09:35 -0500

    Chore--Update-YAGSL-to-2025.2.2 (#11)
    * refactor: use 2025.2.2
    
    * chore: update GradleRIO plugin version to 2025.2.1
    
    * chore: update VSCode settings to increase Java VM memory allocation

--- src/main/java/frc/robot/subsystems/drive/Vision.java
+++ src/main/java/frc/robot/subsystems/drive/Vision.java
@@ -49,7 +49,7 @@ public class Vision {
 
   /** April Tag Field Layout of the year. */
   public static final AprilTagFieldLayout fieldLayout =
-      AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
+      AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
   /** Ambiguity defined as a value between (0,1). Used in {@link Vision#filterPose}. */
   private final double maximumAmbiguity = 0.25;
   /** Photon Vision Simulation */
@@ -79,7 +79,7 @@ public class Vision {
         c.addToVisionSim(visionSim);
       }
 
-      // openSimCameraViews();
+      openSimCameraViews();
     }
   }
 
@@ -279,32 +279,41 @@ public class Vision {
 
   /** Camera Enum to select each camera */
   enum Cameras {
-    /** Left Camera */
-    LEFT_CAM(
-        "left",
-        new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(30)),
-        new Translation3d(
-            Units.inchesToMeters(12.056), Units.inchesToMeters(10.981), Units.inchesToMeters(8.44)),
-        VecBuilder.fill(4, 4, 8),
-        VecBuilder.fill(0.5, 0.5, 1)),
-    /** Right Camera */
-    RIGHT_CAM(
-        "right",
-        new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(-30)),
-        new Translation3d(
-            Units.inchesToMeters(12.056),
-            Units.inchesToMeters(-10.981),
-            Units.inchesToMeters(8.44)),
-        VecBuilder.fill(4, 4, 8),
-        VecBuilder.fill(0.5, 0.5, 1)),
-    /** Center Camera */
-    CENTER_CAM(
-        "center",
-        new Rotation3d(0, Units.degreesToRadians(18), 0),
-        new Translation3d(
-            Units.inchesToMeters(-4.628),
-            Units.inchesToMeters(-10.687),
-            Units.inchesToMeters(16.129)),
+    // /** Left Camera */
+    // LEFT_CAM(
+    //     "left",
+    //     new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(30)),
+    //     new Translation3d(
+    //         Units.inchesToMeters(12.056), Units.inchesToMeters(10.981),
+    // Units.inchesToMeters(8.44)),
+    //     VecBuilder.fill(4, 4, 8),
+    //     VecBuilder.fill(0.5, 0.5, 1)),
+    // /** Right Camera */
+    // RIGHT_CAM(
+    //     "right",
+    //     new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(-30)),
+    //     new Translation3d(
+    //         Units.inchesToMeters(12.056),
+    //         Units.inchesToMeters(-10.981),
+    //         Units.inchesToMeters(8.44)),
+    //     VecBuilder.fill(4, 4, 8),
+    //     VecBuilder.fill(0.5, 0.5, 1)),
+    // /** Center Camera */
+    // CENTER_CAM(
+    //     "center",
+    //     new Rotation3d(0, Units.degreesToRadians(18), 0),
+    //     new Translation3d(
+    //         Units.inchesToMeters(-4.628),
+    //         Units.inchesToMeters(-10.687),
+    //         Units.inchesToMeters(16.129)),
+    //     VecBuilder.fill(4, 4, 8),
+    //     VecBuilder.fill(0.5, 0.5, 1));
+
+    /** Dart - Back Camera */
+    DART_BACK_CAM(
+        "Arducam_AT002",
+        new Rotation3d(0, Units.degreesToRadians(-30), Units.degreesToRadians(167.04746)),
+        new Translation3d(-0.222275, -0.276124, 0.251296),
         VecBuilder.fill(4, 4, 8),
         VecBuilder.fill(0.5, 0.5, 1));
 
