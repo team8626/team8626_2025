2130f4ee4bb36bc4edb3ff59649d80cc0226e6a9
Cyber Sailors <team8626.sw@gmail.com>
2025-01-25 21:42:35 -0500

    chore: move tuning commands out of setters

--- src/main/java/frc/robot/commands/tuning/Tune_CoralShooter.java
+++ src/main/java/frc/robot/commands/tuning/Tune_CoralShooter.java
@@ -4,7 +4,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.setters.tuning;
+package frc.robot.commands.tuning;
 
 import frc.robot.RobotContainer;
 import frc.robot.commands.CS_Command;
