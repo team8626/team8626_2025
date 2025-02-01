f7735231398b8074889e5b3df80f305eb98becb1
Cyber Sailors <team8626.sw@gmail.com>
2025-02-01 17:42:41 -0500

    refactor: comment out unused vision constants in RobotConstants

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -1,14 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 
 /**
@@ -50,28 +41,28 @@ public class RobotConstants {
 
   public static double FrameLength = Units.inchesToMeters(24);
 
-  /** Defines position of the cameras on the robot. */
-  public static class Vision {
-    // April tag camera constants
-    public static final String kATBackCameraName = "Arducam_AT002";
-    public static final Transform3d kATRobotToBackCam =
-        new Transform3d(
-            new Translation3d(-0.222275, -0.276124, 0.251296),
-            new Rotation3d(0, Units.degreesToRadians(-30), Units.degreesToRadians(167.04746)));
+  // /** Defines position of the cameras on the robot. */
+  //   public static class Vision {
+  //     // April tag camera constants
+  //     public static final String kATBackCameraName = "Arducam_AT002";
+  //     public static final Transform3d kATRobotToBackCam =
+  //         new Transform3d(
+  //             new Translation3d(-0.222275, -0.276124, 0.251296),
+  //             new Rotation3d(0, Units.degreesToRadians(-30), Units.degreesToRadians(167.04746)));
 
-    public static final String kATFrontCameraName = "Arducam_AT001";
-    public static final Transform3d kATRobotToFrontCam =
-        new Transform3d(
-            new Translation3d(0.197621, 0.005613, 0.504500),
-            new Rotation3d(0, Units.degreesToRadians(-19.562757), Units.degreesToRadians(17)));
+  //     public static final String kATFrontCameraName = "Arducam_AT001";
+  //     public static final Transform3d kATRobotToFrontCam =
+  //         new Transform3d(
+  //             new Translation3d(0.197621, 0.005613, 0.504500),
+  //             new Rotation3d(0, Units.degreesToRadians(-19.562757), Units.degreesToRadians(17)));
 
-    // The layout of the AprilTags on the field
-    public static final AprilTagFieldLayout kTagLayout =
-        AprilTagFields.kDefaultField.loadAprilTagLayoutField();
+  //     // The layout of the AprilTags on the field
+  //     public static final AprilTagFieldLayout kTagLayout =
+  //         AprilTagFields.kDefaultField.loadAprilTagLayoutField();
 
-    // The standard deviations of our vision estimated poses, which affect correction rate
-    // (Fake values. Experiment and determine estimation noise on an actual robot.)
-    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
-    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
-  }
+  //     // The standard deviations of our vision estimated poses, which affect correction rate
+  //     // (Fake values. Experiment and determine estimation noise on an actual robot.)
+  //     public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
+  //     public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
+  //   }
 }
