7a653d93e67ce3a9edb6da85068fab478a5bb157
Sylvain Dumet <Sylvain.Dumet@gmail.com>
2025-02-17 12:44:59 +0100

    feat: Climber implementation & simulation (#28)
    * feat: implement Climber subsystem interface and simulation classes
    
    * pushing the climber code (still broken)
    
    * refactor: update Climber subsystem interface and constants for angle control
    
    * Merge branch 'main' into feat--climber

---
+++ src/main/java/frc/robot/subsystems/climber/ClimberConstants.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.climber;
+
+import frc.robot.RobotConstants;
+
+public class ClimberConstants {
+
+  // Climber Constants
+  public static final double minAngleDegrees = 0;
+  public static final double maxAngleDegrees = 270;
+  public static final double restAngleDegrees = 0;
+  public static final double positionConversionFactor = 360; // degrees
+  public static final double velocityConversionFactor = 360; // degrees per second
+
+  // Arm Config
+  public static final ArmConfig armConfig =
+      switch (RobotConstants.robotType) {
+        case COMPBOT -> new ArmConfig(13, 12, (125 * 1.5) / 1);
+        case DEVBOT -> new ArmConfig(13, 12, (125 * 1.5) / 1);
+        case SIMBOT -> new ArmConfig(13, 12, (125 * 1.5) / 1);
+        default -> new ArmConfig(0, 12, (125 * 1.5) / 1);
+      };
+
+  // PID Constants
+  public static final Gains gains =
+      switch (RobotConstants.robotType) {
+        case COMPBOT -> new Gains(0.0001, 0.0, 0.0, 0.12, 0.00635, 0);
+        case DEVBOT -> new Gains(0.0001, 0.0, 0.0, 0.12, 0.00635, 0.0);
+        case SIMBOT -> new Gains(0.05, 0.0, 0.0, 0.12, 0.00635, 0.0);
+        default -> new Gains(0.05, 0.0, 0.0, 0.12, 0.00635, 0.0);
+      };
+
+  public record Gains(double kP, double kI, double kD, double kS, double kV, double kA) {}
+
+  public record ArmConfig(int CANID, double armLengthInches, double reduction) {}
+}
---
+++ src/main/java/frc/robot/subsystems/climber/Climber_SparkMax.java
@@ -0,0 +1,110 @@
+package frc.robot.subsystems.climber;
+
+import static frc.robot.subsystems.climber.ClimberConstants.armConfig;
+import static frc.robot.subsystems.climber.ClimberConstants.gains;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import frc.robot.subsystems.CS_InterfaceBase;
+
+public class Climber_SparkMax implements ClimberInterface, CS_InterfaceBase {
+
+  private SparkMax motor;
+
+  private SparkMaxConfig config;
+
+  private final SparkClosedLoopController controller;
+  private AbsoluteEncoder encoder;
+  private final AbsoluteEncoderConfig encoderConfig = new AbsoluteEncoderConfig();
+  private double setPointDegrees;
+
+  // done by ai, idk if it works, but it makes public Climber_SparkMax() not throw an error
+  SimpleMotorFeedforward FF = new SimpleMotorFeedforward(gains.kS(), gains.kV(), gains.kA());
+
+  private boolean climberIsEnabled = false;
+
+  public Climber_SparkMax() {
+    // Setup configuration for the encoder
+    encoderConfig
+        .positionConversionFactor(ClimberConstants.positionConversionFactor)
+        .velocityConversionFactor(ClimberConstants.velocityConversionFactor);
+
+    // Setup configuration for the motor
+    config = new SparkMaxConfig();
+
+    config.inverted(false).idleMode(IdleMode.kBrake).smartCurrentLimit(40);
+
+    config
+        .closedLoop
+        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+        // Set PID values for position control.
+        .p(ClimberConstants.gains.kP())
+        .i(ClimberConstants.gains.kI())
+        .d(ClimberConstants.gains.kD())
+        .outputRange(-1, 1);
+
+    config.apply(encoderConfig);
+
+    motor = new SparkMax(armConfig.CANID(), MotorType.kBrushless);
+    motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    encoder = motor.getAbsoluteEncoder();
+
+    controller = motor.getClosedLoopController();
+    controller.setReference(0, ControlType.kDutyCycle);
+  }
+
+  @Override
+  public void updateInputs(ClimberValues values) {
+    values.climberIsEnabled = climberIsEnabled;
+    values.currentAngleDegrees = getAngleDegrees();
+    values.amps = motor.getOutputCurrent();
+
+    controller.setReference(setPointDegrees, ControlType.kPosition);
+  }
+
+  public double getAngleDegrees() {
+    return encoder.getPosition();
+  }
+
+  // added to fix error at top of class (im hope this doesn't break anything)
+  @Override
+  public void setAngleDegrees(double new_angle) {
+    setPointDegrees = new_angle;
+  }
+
+  // @Override
+  // public void start(double new_setpoint) {
+  //   controller.setReference(new_setpoint, ControlType.kDutyCycle);
+  //   climberIsEnabled = true;
+  // }
+
+  // @Override
+  // public void stop() {
+  //   controller.setReference(0, ControlType.kDutyCycle);
+  //   climberIsEnabled = false;
+  // }
+
+  @Override
+  public void setPID(double newkP, double newkI, double newkD) {
+    config.closedLoop.p(newkP).i(newkI).d(newkD);
+    motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    printf("New PID: %f, %f, %f", newkP, newkI, newkD);
+  }
+
+  @Override
+  public void runCharacterization(double input) {
+    motor.setVoltage(input);
+  }
+}
