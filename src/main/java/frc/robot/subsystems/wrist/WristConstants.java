cae1a79167d4061f665a1d19ba66c521e76132cd
Sylvain Dumet <Sylvain.Dumet@gmail.com>
2025-02-18 12:07:02 +0100

    feat: wrist implementation (#30)
    * feat: implement Wrist subsystem with interface, simulation, and constants
    
    * refactor: simplify LED constants and remove unused error handling code (#23)
    
    * feat: add LED spacing constant and update rainbow parameters
    
    * refactor: simplify LED constants and remove unused error handling code
    
    * Merge remote-tracking branch 'origin/main' into feat--new-LED-impl;ementation
    
    * feat: algaeShooter Commands (#24)
    
    * fest: merge LEDmanager to main (#25)
    
    * Feat--Visualization (#26)
    
    * feat: add configuration files for 3d model
    
    * feat: add pose offsets for robot components in RobotConstants
    
    * feat: add getPose3d method to drive subsystem interfaces
    
    * feat: add Visualization instance to RobotContainer
    
    * feat: implement Visualization subsystem for robot  display on AdvantageScope
    
    * feat: implement Elevator_SimulationRose and update RobotContainer to use it (#27)
    
    * feat: Climber implementation & simulation (#28)
    
    * feat: implement Climber subsystem interface and simulation classes
    
    * pushing the climber code (still broken)
    
    * refactor: update Climber subsystem interface and constants for angle control
    
    * Merge branch 'main' into feat--climber
    
    * refactor: remove unused dummy commands and clean up imports in Commodore and RobotContainer (#29)
    
    * feat: wrist simulation and update subsystem
    
    * Merge branch 'main' into feat--Wrist

---
+++ src/main/java/frc/robot/subsystems/wrist/WristConstants.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems.wrist;
+
+import edu.wpi.first.math.util.Units;
+import frc.robot.RobotConstants;
+
+public class WristConstants {
+
+  public static final double minAngleDegrees = 0;
+  public static final double maxAngleDegrees = 270;
+  public static final double defaultAngleDegrees = 0; // TODO get a good value
+
+  public static final double inputGear = 16;
+  public static final double stageOneInput = 56;
+  public static final double stageOneOutput = 45;
+  public static final double stageTwoInput = 40;
+  public static final double stageTwoOutput = 16;
+  public static final double outputGear = 45;
+  public static final double maxPlanetaryRatio = 16 / 1;
+  public static final double gearBoxRatio =
+      (stageOneInput / inputGear)
+          * (stageTwoInput / stageOneOutput)
+          * (outputGear / stageTwoOutput)
+          * maxPlanetaryRatio;
+
+  public static final double armLength = Units.inchesToMeters(9.5);
+  public static final double armMass = Units.lbsToKilograms(13.5);
+
+  // Wrist Config
+  public static final WristConfig wristConfig =
+      switch (RobotConstants.robotType) {
+        case COMPBOT -> new WristConfig(4, gearBoxRatio, Units.inchesToMeters(9.5));
+        case DEVBOT -> new WristConfig(4, gearBoxRatio, Units.inchesToMeters(9.5));
+        case SIMBOT -> new WristConfig(4, gearBoxRatio, Units.inchesToMeters(9.5));
+        default -> new WristConfig(0, gearBoxRatio, Units.inchesToMeters(9.5));
+      };
+
+  // PID Constants
+  public static final Gains gains =
+      switch (RobotConstants.robotType) {
+        case COMPBOT -> new Gains(0.0001, 0.0, 0.0, 0.12, 0.00635, 0);
+        case DEVBOT -> new Gains(0.0001, 0.0, 0.0, 0.12, 0.00635, 0.0);
+        case SIMBOT -> new Gains(0.05, 0.0, 0.0, 0.12, 0.00635, 0.0);
+        default -> new Gains(0.05, 0.0, 0.0, 0.12, 0.00635, 0.0);
+      };
+
+  public record Gains(double kP, double kI, double kD, double kS, double kV, double kA) {}
+
+  public record WristConfig(int CANID, double reduction, double armLengthInches) {}
+}
