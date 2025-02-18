3f485e78b2af195bc4fac4c0f6ff2628c403c00e
Sylvain Dumet <Sylvain.Dumet@gmail.com>
2025-02-18 12:07:02 +0100

    feat: wrist implementation (#30)
    * feat: implement Wrist subsystem with interface, simulation, and constants
    
    * refactor: simplify LED constants and remove unused error handling code (#23)
    
    * feat: add LED spacing constant and update rainbow parameters
    
    * refactor: simplify LED constants and remove unused error handling code
    
    * Merge remote-tracking branch 'origin/main' into feat--new-LED-impl;ementation
    
    * feat: algaeShooter Commands (#24)
    
    * fest: merge LEDmanager to main (#25)
    
    * Feat--Visualization (#26)
    
    * feat: add configuration files for 3d model
    
    * feat: add pose offsets for robot components in RobotConstants
    
    * feat: add getPose3d method to drive subsystem interfaces
    
    * feat: add Visualization instance to RobotContainer
    
    * feat: implement Visualization subsystem for robot  display on AdvantageScope
    
    * feat: implement Elevator_SimulationRose and update RobotContainer to use it (#27)
    
    * feat: Climber implementation & simulation (#28)
    
    * feat: implement Climber subsystem interface and simulation classes
    
    * pushing the climber code (still broken)
    
    * refactor: update Climber subsystem interface and constants for angle control
    
    * Merge branch 'main' into feat--climber
    
    * refactor: remove unused dummy commands and clean up imports in Commodore and RobotContainer (#29)
    
    * feat: wrist simulation and update subsystem
    
    * Merge branch 'main' into feat--Wrist

---
+++ src/main/java/frc/robot/subsystems/wrist/WristInterface.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.wrist;
+
+public interface WristInterface {
+  void setAngleDegrees(double new_Angle);
+
+  double getAngleDegrees();
+
+  /** Run arm pivot at voltage */
+  default void runCharacterization(double input) {}
+
+  default void setPID(double kP, double kI, double kD) {}
+
+  default void updateInputs(WristValues values) {
+    // Default implementation
+  }
+
+  public class WristValues {
+    protected boolean isEnabled = false;
+    protected double currentAngleDegrees = 0; // Degrees
+    protected double amps = 0;
+
+    // protected double desiredAngleDegrees = 0;
+
+    protected double kP = 0.05;
+    protected double kI = 0.0;
+    protected double kD = 0.0;
+    protected double FF = 0.0;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/wrist/Wrist_SparkFlex.java
@@ -0,0 +1,109 @@
+package frc.robot.subsystems.wrist;
+
+import static frc.robot.subsystems.coralshooter.CoralShooterConstants.gains;
+import static frc.robot.subsystems.wrist.WristConstants.wristConfig;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import frc.robot.subsystems.CS_InterfaceBase;
+import frc.robot.subsystems.climber.ClimberConstants;
+
+public class Wrist_SparkFlex implements WristInterface, CS_InterfaceBase {
+
+  private SparkFlex motor;
+  private SparkFlexConfig config;
+
+  private final SparkClosedLoopController controller;
+  private AbsoluteEncoder encoder;
+  private final AbsoluteEncoderConfig encoderConfig = new AbsoluteEncoderConfig();
+  private double setPointDegrees;
+
+  SimpleMotorFeedforward leftFF = new SimpleMotorFeedforward(gains.kS(), gains.kV(), gains.kA());
+
+  private boolean isEnabled = false;
+
+  public Wrist_SparkFlex() {
+    // Setup configuration for the encoder
+    encoderConfig
+        .positionConversionFactor(ClimberConstants.positionConversionFactor)
+        .velocityConversionFactor(ClimberConstants.velocityConversionFactor);
+
+    // Setup configuration for the motor
+    config = new SparkFlexConfig();
+
+    config.inverted(false).idleMode(IdleMode.kBrake).smartCurrentLimit(40);
+
+    config
+        .closedLoop
+        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+        // Set PID values for position control.
+        .p(ClimberConstants.gains.kP())
+        .i(ClimberConstants.gains.kI())
+        .d(ClimberConstants.gains.kD())
+        .outputRange(-1, 1);
+
+    config.apply(encoderConfig);
+
+    motor = new SparkFlex(wristConfig.CANID(), MotorType.kBrushless);
+    motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    encoder = motor.getAbsoluteEncoder();
+
+    controller = motor.getClosedLoopController();
+    controller.setReference(0, ControlType.kDutyCycle);
+  }
+
+  @Override
+  public void updateInputs(WristValues values) {
+    values.isEnabled = isEnabled;
+    values.currentAngleDegrees = getAngleDegrees();
+    values.amps = motor.getOutputCurrent();
+
+    controller.setReference(setPointDegrees, ControlType.kPosition);
+  }
+
+  public double getAngleDegrees() {
+    return encoder.getPosition();
+  }
+
+  // added to fix error at top of class (im hope this doesn't break anything)
+  @Override
+  public void setAngleDegrees(double new_angle) {
+    setPointDegrees = new_angle;
+  }
+
+  // @Override
+  // public void start(double new_setpoint) {
+  //   controller.setReference(new_setpoint, ControlType.kDutyCycle);
+  //   isEnabled = true;
+  // }
+
+  // @Override
+  // public void stop() {
+  //   controller.setReference(0, ControlType.kDutyCycle);
+  //   isEnabled = false;
+  // }
+
+  @Override
+  public void setPID(double newkP, double newkI, double newkD) {
+    config.closedLoop.p(newkP).i(newkI).d(newkD);
+    motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    printf("New PID: %f, %f, %f", newkP, newkI, newkD);
+  }
+
+  @Override
+  public void runCharacterization(double input) {
+    motor.setVoltage(input);
+  }
+}
