0aa2bc903111f8a4fe3a47d8652216d18821c513
Cyber Sailors <team8626.sw@gmail.com>
2025-03-06 17:02:22 -0500

    refactor: simplify CoralPreset and AlgaePreset classes by removing unused variables and methods

--- src/main/java/frc/robot/subsystems/presets/CoralPreset.java
+++ src/main/java/frc/robot/subsystems/presets/CoralPreset.java
@@ -1,76 +1,76 @@
 package frc.robot.subsystems.presets;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import frc.robot.RobotConstants.UIConstants.CoralLevel;
 import frc.robot.subsystems.coralshooter.CoralShooterConstants;
-import java.util.function.Supplier;
 
 public class CoralPreset {
   public String name = "-";
-  public Supplier<Pose2d> robotPoseSupplier;
+  public Pose2d robotPose;
   public double RPMLeft = CoralShooterConstants.RPMShootLeft;
   public double RPMRight = CoralShooterConstants.RPMShootRight;
-  public boolean isReady = false;
+  // public boolean isReady = false;
+  public boolean hasPose = false;
+  public boolean hasLevel = false;
 
   public CoralPreset(String name) {
     this.name = name;
-    this.isReady = false;
   }
 
-  public CoralPreset(
-      String name, Pose2d robotPose, double RPMLeft, double RPMRight, boolean isReady) {
-    this.name = name;
-    this.robotPoseSupplier = () -> robotPose;
-    this.RPMLeft = RPMLeft;
-    this.RPMRight = RPMRight;
-    this.isReady = isReady;
-  }
+  // public CoralPreset(
+  //     String name, Pose2d robotPose, double RPMLeft, double RPMRight, boolean isReady) {
+  //   this.name = name;
+  //   this.robotPoseSupplier = () -> robotPose;
+  //   this.RPMLeft = RPMLeft;
+  //   this.RPMRight = RPMRight;
+  //   this.isReady = isReady;
+  // }
 
   public CoralPreset(String name, double RPMLeft, double RPMRight) {
     this.name = name;
     this.RPMLeft = RPMLeft;
     this.RPMRight = RPMRight;
-    this.isReady = true;
+    this.hasLevel = true;
+    this.hasPose = true;
   }
 
   public void setPose(Pose2d newPose) {
     // Set the Pose of the Robot
-    this.robotPoseSupplier = () -> newPose;
+    this.robotPose = newPose;
+    this.hasPose = true;
     System.out.printf(
-        "[CORAL PRESET] New Pose Set to - x: %3f, y: %3f, theta: %3f",
-        robotPoseSupplier.get().getX(),
-        robotPoseSupplier.get().getY(),
-        robotPoseSupplier.get().getRotation().getDegrees());
+        "[CORALPRESET] New Pose Set to - x: %3f, y: %3f, theta: %3f\n",
+        robotPose.getX(), robotPose.getY(), robotPose.getRotation().getDegrees());
   }
 
-  public void setSubsystems(CoralLevelPreset newLevel) {
+  public void setLevel(CoralLevel newLevel) {
     // Set the Level of the Coral Shooter
     switch (newLevel) {
       case L1:
         this.RPMLeft = Presets.CORAL_L1.RPMLeft;
         this.RPMRight = Presets.CORAL_L1.RPMRight;
+        this.hasLevel = true;
         break;
       case L2:
         this.RPMLeft = Presets.CORAL_L2.RPMLeft;
         this.RPMRight = Presets.CORAL_L2.RPMRight;
+        this.hasLevel = true;
         break;
       case L3:
         this.RPMLeft = Presets.CORAL_L3.RPMLeft;
         this.RPMRight = Presets.CORAL_L3.RPMRight;
+        this.hasLevel = true;
         break;
       case L4:
       default:
         this.RPMLeft = Presets.CORAL_L4.RPMLeft;
         this.RPMRight = Presets.CORAL_L4.RPMRight;
+        this.hasLevel = true;
         break;
     }
     System.out.printf(
         "[CORALPRESET] New Coral Preset - %s - %.0f/%.0f RPM\n",
         newLevel.toString(), this.RPMLeft, this.RPMRight);
-  }
-
-  public void setReady(boolean ready) {
-    // Set the Ready State of the Preset
-    this.isReady = ready;
   }
 
   public double getRPMLeft() {
@@ -85,31 +85,16 @@ public class CoralPreset {
 
   public Pose2d getPose() {
     // Get the Pose of the Coral Shooter
-    return this.robotPoseSupplier.get();
+    return hasPose ? this.robotPose : null;
   }
 
-  public boolean isReady() {
+  private boolean isReady() {
     // Get the Ready State of the Preset
-    return this.isReady;
-  }
-}
-
-enum CoralLevelPreset {
-  L1(900, 1000),
-  L2(500, 500),
-  L3(500, 500),
-  L4(1275, 1275);
-
-  CoralLevelPreset(int RMPLeft, int RMPRight) {}
-
-  public double RPMLeft() {
-    return RPMLeft;
+    return this.hasLevel && this.hasPose;
   }
 
-  public double RPMRight() {
-    return RPMRight;
+  public void reset() {
+    // Set the Ready State of the Preset
+    this.hasPose = false;
   }
-
-  private static double RPMLeft;
-  private static double RPMRight;
 }
--- src/main/java/frc/robot/subsystems/presets/PresetManager.java
+++ src/main/java/frc/robot/subsystems/presets/PresetManager.java
@@ -8,6 +8,8 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.RobotConstants;
 import frc.robot.RobotConstants.UIConstants;
 import frc.robot.RobotConstants.UIConstants.AlgaeFace2;
@@ -17,34 +19,22 @@ import frc.robot.RobotConstants.UIConstants.DTP;
 import frc.robot.RobotConstants.UIConstants.PickupSide;
 import frc.robot.subsystems.CS_SubsystemBase;
 import frc.robot.subsystems.Dashboard;
-import org.littletonrobotics.frc2025.FieldConstants.CoralObjective;
 import org.littletonrobotics.frc2025.FieldConstants.Reef;
 import org.littletonrobotics.frc2025.FieldConstants.ReefLevel;
 import org.littletonrobotics.frc2025.util.AllianceFlipUtil;
 
 public class PresetManager extends CS_SubsystemBase {
-  private String uiSelectedCoralLevel = "";
-  private String uiSelectedIntakeSide = "";
-  private String uiSelectedCoralBranch = "";
-  private String uiSelectedAlgaeFace = "";
-
   private UIConstants.CoralLevel uiSelectedCoralLevel2 = UIConstants.defaultCoralLevel;
   private UIConstants.PickupSide uiSelectedIntakeSide2 = UIConstants.defaultPickupSide;
   private UIConstants.CoralBranch uiSelectedCoralBranch2 = CoralBranch.NONE;
-  private UIConstants.AlgaeFace2 uiSelectedAlgaeFace2 = AlgaeFace2.NONE;
+  private UIConstants.AlgaeFace2 uiSelectedAlgaeFace2 = AlgaeFace2.FLOOR;
   private UIConstants.DTP uiSelectedDtp2 = UIConstants.defaultDTP;
 
   private UIConstants.CoralLevel uiCurrentCoralLevel2 = UIConstants.defaultCoralLevel;
   private UIConstants.PickupSide uiCurrentIntakeSide2 = UIConstants.defaultPickupSide;
   private UIConstants.CoralBranch uiCurrentCoralBranch2 = CoralBranch.NONE;
-  private UIConstants.AlgaeFace2 uiCurrentAlgaeFace2 = AlgaeFace2.NONE;
+  private UIConstants.AlgaeFace2 uiCurrentAlgaeFace2 = AlgaeFace2.FLOOR;
   private static UIConstants.DTP uiCurrentDtp2 = UIConstants.defaultDTP;
-
-  private String uiCurrentCoralLevel = "L4";
-  private String uiCurrentIntakeSide = "----";
-  private String uiCurrentCoralBranch = "-";
-  private String uiCurrentAlgaeFace = "--";
-  private boolean uiCurrentUseDtp = UIConstants.defaultDTP.getValue();
 
   // private final StringPublisher coralLevelPub =
   // NetworkTableInstance.getDefault().getStringTopic("SmartDashboard/Presets/UI/SelectedCoralLevel");
@@ -67,6 +57,10 @@ public class PresetManager extends CS_SubsystemBase {
 
   private PresetManager() {
     super();
+
+    // Initialize the Presets
+    currentCoralPreset.setLevel(uiSelectedCoralLevel2);
+    currentAlgaePreset.setSubsystems(uiSelectedAlgaeFace2);
   }
 
   @Override
@@ -78,12 +72,10 @@ public class PresetManager extends CS_SubsystemBase {
 
   // Return the current presets
   public static CoralPreset getCoralPreset() {
-    // TODO: Return null if not ready or have an isCoralPresetReady() method
     return currentCoralPreset;
   }
 
   public static AlgaePreset getAlgaePreset() {
-    // TODO: Return null if not ready or have an isAlgaePresetReady() method
     return currentAlgaePreset;
   }
 
@@ -194,24 +186,28 @@ public class PresetManager extends CS_SubsystemBase {
     CoralLevel coralLevel = Dashboard.getSelectedCoralLevel();
     if (coralLevel != null) {
       uiSelectedCoralLevel2 = coralLevel;
+      // Will be processed in UdatePresets()
       printf("Coral Level: %s", uiSelectedCoralLevel2.toString());
     }
 
     CoralBranch coralBranch = Dashboard.getSelectedCoralBranch();
     if (coralBranch != null) {
       uiSelectedCoralBranch2 = coralBranch;
+      // Will be processed in UdatePresets()
       printf("Coral Branch: %s", uiSelectedCoralBranch2.toString());
     }
 
     AlgaeFace2 algaeFace = Dashboard.getSelectedAlgaeFace();
     if (algaeFace != null) {
       uiSelectedAlgaeFace2 = algaeFace;
+      // Will be processed in UdatePresets()
       printf("Algae: %s", uiSelectedAlgaeFace2.toString());
     }
 
     PickupSide intakeSide = Dashboard.getSelectedPickupSide();
     if (intakeSide != null) {
       uiSelectedIntakeSide2 = intakeSide;
+      uiCurrentIntakeSide2 = uiSelectedIntakeSide2;
       printf("Intake Side: %s", uiSelectedIntakeSide2.toString());
     }
 
@@ -219,190 +215,105 @@ public class PresetManager extends CS_SubsystemBase {
     if (dtp != null) {
       uiSelectedDtp2 = dtp;
       printf("DTP: %s", uiSelectedDtp2.toString());
+      uiCurrentDtp2 = uiSelectedDtp2;
     }
-    // uiSelectedCoralLevel2 = ReefControlsIOServer.getSelectedCoralLevel();
-    // uiSelectedIntakeSide2 = ReefControlsIOServer.getSelectedPickupSide();
-    // uiSelectedCoralBranch2 = ReefControlsIOServer.getSelectedCoralBranch();
-    // uiSelectedAlgaeFace2 = ReefControlsIOServer.getSelectedAlgaeFace();
-
-    // Get Values from UI
-
   }
 
   // Update the Presets based on the UI Data
   private void updatePreset() {
     // New Selected Value for CORAL (Branch or Level)
-    // Compute the new robot preset.
-    // uiSelectedCoralLevel = "L1";
-    // uiSelectedCoralBranch = "F";
-
-    if ((!uiSelectedCoralLevel2.equals(this.uiCurrentCoralLevel2)
-            && UIConstants.allowedCoralLevels2.contains(uiSelectedCoralLevel2))
-        || (!uiSelectedCoralBranch2.equals(this.uiCurrentCoralBranch2)
-            && UIConstants.allowedCoralBranches2.contains(uiSelectedCoralBranch2))) {
-
+    if (!uiSelectedCoralLevel2.equals(this.uiCurrentCoralLevel2)
+        && UIConstants.allowedCoralLevels2.contains(uiSelectedCoralLevel2)) {
       printf(
           "Updating Presets - %s, %s",
-          uiSelectedCoralBranch2.toString(), uiSelectedCoralLevel.toString());
-
-      // New Selected Value for CORAL (Branch or Level)
-      this.uiCurrentCoralBranch2 = uiSelectedCoralBranch2;
+          uiSelectedCoralBranch2.toString(), uiSelectedCoralLevel2.toString());
       this.uiCurrentCoralLevel2 = uiSelectedCoralLevel2;
 
-      Pose2d branchPose =
-          getBranchPoseFromTarget2(this.uiCurrentCoralBranch2, this.uiCurrentCoralLevel2);
+      currentCoralPreset.setLevel(this.uiCurrentCoralLevel2);
+      Dashboard.setSelectedCoralLevel(this.uiSelectedCoralLevel2.getValue());
+    }
 
-      Pose2d robotPose =
-          branchPose.plus(
-              new Transform2d(
-                  RobotConstants.robotCenterOffset.getX()
-                      + Units.inchesToMeters(1.625), //  1.625 for Branch inset to face distance...
-                  0,
-                  branchPose.getRotation()));
+    if (!uiSelectedCoralBranch2.equals(this.uiCurrentCoralBranch2)) {
+      if (UIConstants.allowedCoralBranches.contains(uiSelectedCoralBranch2)) {
+        printf(
+            "Updating Presets - %s, %s",
+            uiSelectedCoralBranch2.toString(), uiSelectedCoralLevel2.toString());
+        this.uiCurrentCoralBranch2 = uiSelectedCoralBranch2;
 
-      printf(
-          "Transform: %f, %f, %f",
-          RobotConstants.robotCenterOffset.getX(), 0.0, robotPose.getRotation().getDegrees());
-      printf(
-          "New Robot Pose - x: %3f, y: %3f, theta: %3f",
-          robotPose.getX(), robotPose.getY(), robotPose.getRotation().getDegrees());
+        Pose2d branchPose =
+            getBranchPoseFromTarget2(this.uiCurrentCoralBranch2, this.uiCurrentCoralLevel2);
 
-      currentCoralPreset.setPose(robotPose);
-      // TODO: Set Coral Level
-    } else {
-      // No new data or no valid data (to be handled)
+        Pose2d robotPose =
+            branchPose.plus(
+                new Transform2d(
+                    RobotConstants.robotCenterOffset.getX()
+                        + Units.inchesToMeters(
+                            1.625), //  1.625 for Branch inset to face distance...
+                    0,
+                    branchPose.getRotation()));
+
+        Pose2d goodRobotPose =
+            new Pose2d(
+                robotPose.getX(),
+                robotPose.getY(),
+                new Rotation2d(branchPose.getRotation().getRadians() + Math.PI));
+
+        printf(
+            "Transform: %f, %f, %f",
+            RobotConstants.robotCenterOffset.getX(), 0.0, robotPose.getRotation().getDegrees());
+        printf(
+            "New Robot Pose - x: %3f, y: %3f, theta: %3f",
+            goodRobotPose.getX(), goodRobotPose.getY(), goodRobotPose.getRotation().getDegrees());
+
+        currentCoralPreset.setPose(AllianceFlipUtil.apply(goodRobotPose));
+
+      } else if (uiSelectedCoralBranch2.equals(CoralBranch.NONE)) {
+        this.uiCurrentCoralBranch2 = uiSelectedCoralBranch2;
+        currentCoralPreset.reset();
+      }
+      Dashboard.setSelectedCoralBranch(this.uiSelectedCoralBranch2.getValue());
     }
 
     // New Selected Value for ALGAE (Face)
-    uiSelectedAlgaeFace = "IJ";
+    if (!uiSelectedAlgaeFace2.equals(this.uiCurrentAlgaeFace2)) {
+      // New Selected Value for ALGAE is around the REEF
+      if (UIConstants.allowedAlgaeFaces2.contains(uiSelectedAlgaeFace2)) {
 
-    if (!uiSelectedAlgaeFace2.equals(this.uiCurrentAlgaeFace2)
-        && UIConstants.allowedAlgaeFaces2.contains(uiSelectedAlgaeFace2)) {
+        printf("Updating Algae Presets - %s", uiSelectedAlgaeFace2.toString());
 
-      printf("Updating Algae Presets - %s", uiSelectedAlgaeFace2.toString());
+        this.uiCurrentAlgaeFace2 = uiSelectedAlgaeFace2;
 
-      this.uiCurrentAlgaeFace2 = uiSelectedAlgaeFace2;
+        Pose2d facePose = getFacePoseFromTarget2(this.uiCurrentAlgaeFace2);
+        Pose2d robotPose =
+            facePose
+                .plus(
+                    new Transform2d(
+                        RobotConstants.robotCenterOffset.getX(), 0, (facePose.getRotation())))
+                .rotateBy(new Rotation2d(Math.PI));
+        Pose2d goodRobotPose =
+            new Pose2d(
+                robotPose.getX(),
+                robotPose.getY(),
+                new Rotation2d(facePose.getRotation().getRadians()));
 
-      Pose2d facePose = getFacePoseFromTarget2(this.uiCurrentAlgaeFace2);
-      Pose2d robotPose =
-          facePose
-              .plus(
-                  new Transform2d(
-                      RobotConstants.robotCenterOffset.getX(), 0, (facePose.getRotation())))
-              .rotateBy(new Rotation2d(Math.PI));
-      Pose2d goodRobotPose =
-          new Pose2d(
-              robotPose.getX(),
-              robotPose.getY(),
-              new Rotation2d(facePose.getRotation().getRadians()));
+        printf(
+            "New Algae Pose - x: %3f, y: %3f, theta: %3f",
+            goodRobotPose.getX(), goodRobotPose.getY(), goodRobotPose.getRotation().getDegrees());
 
-      printf(
-          "New Algae Pose - x: %3f, y: %3f, theta: %3f",
-          goodRobotPose.getX(), goodRobotPose.getY(), goodRobotPose.getRotation().getDegrees());
+        currentAlgaePreset.setPose(AllianceFlipUtil.apply(goodRobotPose));
+        currentAlgaePreset.setSubsystems(uiSelectedAlgaeFace2);
+      }
+      // New Selected Value for ALGAE is around the REEF ==> FLOOR
+      else if (uiSelectedAlgaeFace2.equals(AlgaeFace2.FLOOR)) {
+        printf("Updating Algae Presets - %s", uiSelectedAlgaeFace2.toString());
 
-      currentAlgaePreset.setPose(goodRobotPose);
+        this.uiCurrentAlgaeFace2 = uiSelectedAlgaeFace2;
+        currentAlgaePreset.reset();
+      }
+      Dashboard.setSelectedAlgaeFace(this.uiCurrentAlgaeFace2.getValue());
     } else {
       // No new data or no valid data (to be handled)
     }
-  }
-
-  private void updatePresetOLD() {
-    // New Selected Value for CORAL (Branch or Level)
-    // Compute the new robot preset.
-    // uiSelectedCoralLevel = "L1";
-    // uiSelectedCoralBranch = "F";
-
-    if ((!uiSelectedCoralLevel.equals(this.uiCurrentCoralLevel)
-            && UIConstants.allowedCoralLevels.contains(uiSelectedCoralLevel))
-        || (!uiSelectedCoralBranch.equals(this.uiCurrentCoralBranch)
-            && UIConstants.allowedCoralBranches.contains(uiSelectedCoralBranch))) {
-
-      printf("Updating Presets - %s, %s", uiSelectedCoralBranch, uiSelectedCoralLevel);
-
-      // New Selected Value for CORAL (Branch or Level)
-      this.uiCurrentCoralBranch = uiSelectedCoralBranch;
-      this.uiCurrentCoralLevel = uiSelectedCoralLevel;
-
-      Pose2d branchPose =
-          getBranchPoseFromTarget(this.uiCurrentCoralBranch, this.uiCurrentCoralLevel);
-
-      Pose2d robotPose =
-          branchPose.plus(
-              new Transform2d(
-                  RobotConstants.robotCenterOffset.getX()
-                      + Units.inchesToMeters(1.625), //  1.625 for Branch inset to face distance...
-                  0,
-                  branchPose.getRotation()));
-      printf(
-          "New Coral Pose - x: %3f, y: %3f, theta: %3f",
-          robotPose.getX(), robotPose.getY(), branchPose.getRotation().getDegrees());
-
-      currentCoralPreset.setPose(
-          AllianceFlipUtil.apply(
-              new Pose2d(robotPose.getX(), robotPose.getY(), branchPose.getRotation())));
-
-      currentCoralPreset.setSubsystems(CoralLevelPreset.valueOf(uiCurrentCoralLevel));
-      currentCoralPreset.setReady(true);
-    } else {
-      // No new data or no valid data
-    }
-
-    // New Algae Position
-    // Compute the new position ands shooting parameters
-    if (!uiSelectedAlgaeFace.equals(this.uiCurrentAlgaeFace)
-        && UIConstants.allowedAlgaePositions.contains(uiSelectedAlgaeFace)) {
-
-      // printf("Updating Algae Presets - %s", uiSelectedAlgaeFace);
-      // // SmartDashboard.putString("Presets/UI/SelectedCoralLevel", "L3");
-      // String[] abc = {"L1", "L2", "L3", "L4"};
-
-      // // Initialize the Networktable publishers
-      // NetworkTableInstance nt = NetworkTableInstance.getDefault();
-      // NetworkTable stateTable = nt.getTable("/SmartDashboard/Presets/UI");
-      // StringPublisher levelPublisher = stateTable.getStringTopic("SelectedCoralLevel").publish();
-
-      // // Select a random value from the array
-      // String[] someLevels = {"L1", "L2", "L3", "L4"};
-      // Random random = new Random();
-      // String randomCoralLevel = someLevels[random.nextInt(someLevels.length)];
-
-      // SmartDashboard.putStringArray("Presets/UI/AllowedCoralLevels", abc);
-      // // SmartDashboard.putString("Presets/UI/SelectedCoralLevel", randomCoralLevel);
-      // levelPublisher.set(randomCoralLevel);
-      // SmartDashboard.putString("Presets/UI/RobotCoralLevel", randomCoralLevel);
-
-      this.uiCurrentAlgaeFace = uiSelectedAlgaeFace;
-
-      Pose2d facePose = getFacePoseFromTarget(this.uiCurrentAlgaeFace);
-      Pose2d robotPose =
-          facePose
-              .plus(
-                  new Transform2d(
-                      RobotConstants.robotCenterOffset.getX(), 0, (facePose.getRotation())))
-              .rotateBy(new Rotation2d(Math.PI));
-      Pose2d goodRobotPose =
-          new Pose2d(
-              robotPose.getX(),
-              robotPose.getY(),
-              new Rotation2d(facePose.getRotation().getRadians()));
-
-      printf(
-          "New Algae Pose - x: %3f, y: %3f, theta: %3f",
-          goodRobotPose.getX(), goodRobotPose.getY(), goodRobotPose.getRotation().getDegrees());
-
-      currentAlgaePreset.setPose(
-          AllianceFlipUtil.apply(
-              new Pose2d(goodRobotPose.getX(), goodRobotPose.getY(), facePose.getRotation())));
-
-      currentAlgaePreset.setSubsystems(uiSelectedAlgaeFace);
-      currentAlgaePreset.setReady(true);
-    } else {
-      // No new data or no valid data
-    }
-  }
-
-  private static Pose2d getBranchPose(CoralObjective objective) {
-    return Reef.branchPositions.get(objective.branchId()).get(objective.reefLevel()).toPose2d();
   }
 
   private Pose2d getBranchPoseFromTarget2(CoralBranch branch, CoralLevel level) {
@@ -468,70 +379,6 @@ public class PresetManager extends CS_SubsystemBase {
     return Reef.branchPositions.get(branchId).get(branchHeight).toPose2d();
   }
 
-  private Pose2d getBranchPoseFromTarget(String branch, String level) {
-    ReefLevel branchHeight;
-    int branchId = 0;
-
-    switch (branch) {
-      case "L1":
-        branchHeight = ReefLevel.fromLevel(1);
-        break;
-      case "L2":
-        branchHeight = ReefLevel.fromLevel(2);
-        break;
-      case "L3":
-        branchHeight = ReefLevel.fromLevel(3);
-        break;
-      case "4":
-      default:
-        branchHeight = ReefLevel.fromLevel(4);
-    }
-
-    switch (branch) {
-      case "A":
-        branchId = 1;
-        break;
-      case "B":
-        branchId = 0;
-        break;
-      case "C":
-        branchId = 11;
-        break;
-      case "D":
-        branchId = 10;
-        break;
-      case "E":
-        branchId = 9;
-        break;
-      case "F":
-        branchId = 8;
-        break;
-      case "G":
-        branchId = 7;
-        break;
-      case "H":
-        branchId = 6;
-        break;
-      case "I":
-        branchId = 5;
-        break;
-      case "J":
-        branchId = 4;
-        break;
-      case "K":
-        branchId = 3;
-        break;
-      case "L":
-        branchId = 2;
-        break;
-
-      default:
-        branchId = 0;
-        break;
-    }
-    return Reef.branchPositions.get(branchId).get(branchHeight).toPose2d();
-  }
-
   private Pose2d getFacePoseFromTarget2(AlgaeFace2 face) {
     int faceId = 0;
     boolean isLow = false;
@@ -561,47 +408,9 @@ public class PresetManager extends CS_SubsystemBase {
         faceId = 1;
         isLow = true;
         break;
-    }
-
-    printf(
-        "New Face (%s - %s) - x: %3f, y: %3f, theta: %3f",
-        face,
-        isLow ? "Low" : "High",
-        Reef.centerFaces[faceId].getX(),
-        Reef.centerFaces[faceId].getY(),
-        Reef.centerFaces[faceId].getRotation().getDegrees());
-
-    return Reef.centerFaces[faceId].rotateBy(new Rotation2d(Math.PI));
-  }
-
-  private Pose2d getFacePoseFromTarget(String face) {
-    int faceId = 0;
-    boolean isLow = false;
-
-    switch (face) {
-      case "AB":
-        faceId = 0;
+      case FLOOR:
+      default:
         isLow = false;
-        break;
-      case "CD":
-        faceId = 5;
-        isLow = true;
-        break;
-      case "EF":
-        faceId = 4;
-        isLow = false;
-        break;
-      case "GH":
-        faceId = 3;
-        isLow = true;
-        break;
-      case "IJ":
-        faceId = 2;
-        isLow = false;
-        break;
-      case "KL":
-        faceId = 1;
-        isLow = true;
         break;
     }
 
@@ -618,5 +427,13 @@ public class PresetManager extends CS_SubsystemBase {
 
   public static boolean usingDtp() {
     return uiCurrentDtp2.getValue();
+  }
+
+  public static Command resetCoralPresetCmd() {
+    return new InstantCommand(() -> currentCoralPreset.reset());
+  }
+
+  public static Command resetAlgaePresetCmd() {
+    return new InstantCommand(() -> currentAlgaePreset.reset());
   }
 }
