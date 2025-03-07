8879a4320caa56e85803ebcc32911f1b80e10d2a
Cyber Sailors <team8626.sw@gmail.com>
2025-03-06 23:22:35 -0500

    Update ElevatorConstants: adjust height limits and initialization values

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -12,9 +12,9 @@ public class ElevatorConstants {
   public static final double positionConversionFactor =
       1 / (Units.inchesToMeters(2 * Math.PI * drumRadiusInches) * cascadingRatio);
   public static final double velocityConversionFactor = positionConversionFactor / 60;
-  public static final double minHeightInches = 7;
-  public static final double maxHeightInches = 50;
-  public static final double initHeightInches = 7;
+  public static final double minHeightInches = 8;
+  public static final double maxHeightInches = 45;
+  public static final double initHeightInches = 8;
 
   public static final int maxCurrent = 60; // Amps
 
