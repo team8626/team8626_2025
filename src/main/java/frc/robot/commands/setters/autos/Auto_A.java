3318df9d9000bd454d3ea250a233e2e34e8b43fc
Cyber Sailors <team8626.sw@gmail.com>
2025-03-08 14:14:23 -0500

    Add new autonomous command classes: Auto_A, Auto_H, Auto_PickupLeft, and Auto_PickupRight

---
+++ src/main/java/frc/robot/commands/setters/autos/Auto_A.java
@@ -0,0 +1,30 @@
+package frc.robot.commands.setters.autos;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotConstants.UIConstants.CoralBranch;
+import frc.robot.RobotConstants.UIConstants.CoralLevel;
+import frc.robot.subsystems.coralshooter.CoralShooterConstants;
+import frc.robot.subsystems.presets.PresetManager;
+import java.util.function.Supplier;
+
+public class Auto_A extends SequentialCommandGroup {
+
+  private Supplier<Pose2d> poseSupplier;
+
+  public Auto_A() {
+    System.out.printf("[Cmd: %s] Loaded\n", this.getName().toUpperCase());
+    poseSupplier = () -> PresetManager.getCoralPreset().getPose();
+    poseSupplier = () -> PresetManager.getRobotPoseFromTarget(CoralBranch.A, CoralLevel.L4);
+
+    addCommands(
+        // new FollowPathToPose(poseSupplier),
+        // new DriveFinalApproach(poseSupplier),
+        // TODO: UNCOMMENT THIS AND TEST
+        new WaitCommand(CoralShooterConstants.shooterTimerSeconds),
+        new PrintCommand("I AM SHOOOOOOOTING --------!!!!!"));
+    // new ToCoralShoot2());
+  }
+}
