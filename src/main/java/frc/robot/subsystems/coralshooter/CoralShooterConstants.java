d73a09d3814ce69479db912121b533dc3ffb3607
Cyber Sailors <team8626.sw@gmail.com>
2025-03-09 07:50:51 -0400

    feat: Remove TODO comments from CoralShooterConstants for launch and shooter timer seconds

--- src/main/java/frc/robot/subsystems/coralshooter/CoralShooterConstants.java
+++ src/main/java/frc/robot/subsystems/coralshooter/CoralShooterConstants.java
@@ -14,8 +14,8 @@ public class CoralShooterConstants {
 
   public static final double RPMTolerance = 20;
   public static final double RPMDifferentialTolerance = 10;
-  public static final double launchTimerSeconds = 0.2; // TODO: Tune this
-  public static final double shooterTimerSeconds = 0.2; // TODO: Tune this
+  public static final double launchTimerSeconds = 0.2;
+  public static final double shooterTimerSeconds = 0.2;
 
   public static final int maxCurrent = 60; // Amps
 
