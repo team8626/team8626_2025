294cbc5915ded21775bcc60f2ac1107d4fa2b870
Cyber Sailors <team8626.sw@gmail.com>
2025-03-14 22:00:58 -0400

    feat: add new Algae presets for shooting barge positions and test configurations

--- src/main/java/frc/robot/subsystems/presets/Presets.java
+++ src/main/java/frc/robot/subsystems/presets/Presets.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.presets;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+
 public class Presets {
 
   public static final AlgaePreset ALGAE_LOW = new AlgaePreset("LOW", 35, 200, -600);
@@ -13,6 +17,26 @@ public class Presets {
       new AlgaePreset("ALGAE_NETFROM6FT", 8, 180 - 74, 1433);
   public static final AlgaePreset ALGAE_NETFROM10FT =
       new AlgaePreset("ALGAE_NETFROM10FT", 8, 180 - 66, 3000);
+  public static final AlgaePreset ALGAE_Test2 = new AlgaePreset("ALGAE_Test2", 20, 180 - 74, 2500);
+  public static final AlgaePreset ALGAE_Test3 = new AlgaePreset("ALGAE_Test3", 20, 180 - 80, 2500);
+  public static final AlgaePreset ALGAE_Test4 = new AlgaePreset("ALGAE_Test4", 8, 180 - 80, 3000);
+  public static final AlgaePreset ALGAE_Test1 = new AlgaePreset("ALGAE_Test1", 8, 180 - 80, 2700);
+
+  public static final AlgaePreset ALGAE_SHOOTBARGE_OURSIDE =
+      new AlgaePreset(
+          "ALGAE_SHOOTBARGE_OURSIDE",
+          new Pose2d(7, 5, new Rotation2d(Units.degreesToRadians(37))),
+          20,
+          180 - 80,
+          2500);
+
+  public static final AlgaePreset ALGAE_SHOOTBARGE_THEIRSIDE =
+      new AlgaePreset(
+          "ALGAE_SHOOTBARGE_THEIRSIDE",
+          new Pose2d(10.3, 6.5, new Rotation2d(Units.degreesToRadians(-140))),
+          20,
+          180 - 80,
+          2500);
 
   public static final AlgaePreset ALGAE_STOW = new AlgaePreset("STOW", 8, 75, 0);
 
@@ -20,4 +44,7 @@ public class Presets {
   public static final CoralPreset CORAL_L2 = new CoralPreset("L2", 900, 1000);
   public static final CoralPreset CORAL_L3 = new CoralPreset("L3", 550, 1335);
   public static final CoralPreset CORAL_L4 = new CoralPreset("L4", 1300, 1300);
+
+  public static final Pose2d Test3Pose =
+      new Pose2d(7, 5, new Rotation2d(Units.degreesToRadians(37)));
 }
