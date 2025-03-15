a2b0a14619172a5773765898d8c7dc8a68040c79
Cyber Sailors <team8626.sw@gmail.com>
2025-03-14 22:49:54 -0400

    refactor: remove unused PrintCommand import in ToPathAndCoralShoot3

--- src/main/java/frc/robot/commands/setters/groups/ToPathAndCoralShoot3.java
+++ src/main/java/frc/robot/commands/setters/groups/ToPathAndCoralShoot3.java
@@ -3,7 +3,6 @@ package frc.robot.commands.setters.groups;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.setters.units.DriveToPoseFinkle;
 import frc.robot.subsystems.presets.CoralPreset;
