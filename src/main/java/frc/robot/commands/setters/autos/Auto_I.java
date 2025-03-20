f2cdfcedc3b8b449ca5de4ad856de1c30d6e7506
Cyber Sailors <team8626.sw@gmail.com>
2025-03-20 16:13:49 -0400

    fix: update Auto commands to use ToCoralShoot instead of ToCoralShoot3

--- src/main/java/frc/robot/commands/setters/autos/Auto_I.java
+++ src/main/java/frc/robot/commands/setters/autos/Auto_I.java
@@ -4,7 +4,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.UIConstants.CORAL_BRANCH;
 import frc.robot.UIConstants.CORAL_LEVEL;
-import frc.robot.commands.setters.groups.ToCoralShoot3;
+import frc.robot.commands.setters.groups.ToCoralShoot;
 import frc.robot.commands.setters.units.DriveToPoseFinkle;
 import frc.robot.subsystems.presets.PresetManager;
 import java.util.function.Supplier;
@@ -15,6 +15,6 @@ public class Auto_I extends SequentialCommandGroup {
 
   public Auto_I() {
     poseSupplier = () -> PresetManager.getRobotPoseFromTarget(CORAL_BRANCH.I, CORAL_LEVEL.L4, 0);
-    addCommands(new DriveToPoseFinkle(poseSupplier), new ToCoralShoot3());
+    addCommands(new DriveToPoseFinkle(poseSupplier), new ToCoralShoot());
   }
 }
--- src/main/java/frc/robot/commands/setters/autos/Auto_J.java
+++ src/main/java/frc/robot/commands/setters/autos/Auto_J.java
@@ -4,7 +4,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.UIConstants.CORAL_BRANCH;
 import frc.robot.UIConstants.CORAL_LEVEL;
-import frc.robot.commands.setters.groups.ToCoralShoot3;
+import frc.robot.commands.setters.groups.ToCoralShoot;
 import frc.robot.commands.setters.units.DriveToPoseFinkle;
 import frc.robot.subsystems.presets.PresetManager;
 import java.util.function.Supplier;
@@ -15,6 +15,6 @@ public class Auto_J extends SequentialCommandGroup {
 
   public Auto_J() {
     poseSupplier = () -> PresetManager.getRobotPoseFromTarget(CORAL_BRANCH.J, CORAL_LEVEL.L4, 0);
-    addCommands(new DriveToPoseFinkle(poseSupplier), new ToCoralShoot3());
+    addCommands(new DriveToPoseFinkle(poseSupplier), new ToCoralShoot());
   }
 }
