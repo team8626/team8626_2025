6a3de4fa3ee64d6e05b0c065f3d266e9fa977d6c
Cyber Sailors <team8626.sw@gmail.com>
2025-03-24 22:55:36 -0400

    chore: spotless

--- src/main/java/frc/robot/subsystems/elevator/Elevator_LinearSparkMax.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator_LinearSparkMax.java
@@ -156,7 +156,7 @@ public class Elevator_LinearSparkMax implements ElevatorInterface, CS_InterfaceB
 
       // desired height is low and amps are high, chain skipped,
       // that requires to reset...
-      if((desiredHeightInches < 9) && (this.rightMotor.getOutputCurrent() > 30)){
+      if ((desiredHeightInches < 9) && (this.rightMotor.getOutputCurrent() > 30)) {
         // this.desiredHeightInches = ElevatorConstants.initHeightInches;
         encoder.setPosition(ElevatorConstants.initHeightInches);
         rightController.setReference(0, ControlType.kDutyCycle);
