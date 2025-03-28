4526c1595679811caa93ddeeca09f74bc6edc61d
Cyber Sailors <team8626.sw@gmail.com>
2025-03-28 10:57:13 -0400

    refactor: replace ToCoralIntake with CoralShooterIntake in Auto_PickupLeft and Auto_PickupRight commands

--- src/main/java/frc/robot/commands/setters/autos/Auto_PickupRight.java
+++ src/main/java/frc/robot/commands/setters/autos/Auto_PickupRight.java
@@ -5,20 +5,24 @@ import static edu.wpi.first.units.Units.Inches;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
 import frc.robot.UIConstants.PICKUP_SIDE;
-import frc.robot.commands.setters.groups.ToCoralIntake;
+import frc.robot.commands.setters.units.CoralShooterIntake;
 import frc.robot.commands.setters.units.DriveToPoseFinkle2;
+import frc.robot.subsystems.coralshooter.CoralShooterSubsystem;
 import frc.robot.subsystems.presets.PresetManager;
 import java.util.function.Supplier;
 
 public class Auto_PickupRight extends SequentialCommandGroup {
 
   private Supplier<Pose2d> poseSupplier;
+  private CoralShooterSubsystem mortar = RobotContainer.mortar;
 
   public Auto_PickupRight() {
     poseSupplier = () -> PresetManager.getRobotPoseFromPickupSide(() -> PICKUP_SIDE.RIGHT);
     addCommands(
         new DriveToPoseFinkle2(poseSupplier, () -> Inches.of(1), () -> Degrees.of(5)),
-        new ToCoralIntake());
+        // new WaitUntilCommand(() -> mortar.hasCoral() || mortar.isLoaded()),
+        new CoralShooterIntake().onlyIf(() -> !mortar.isLoaded()));
   }
 }
